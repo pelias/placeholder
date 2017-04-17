a1db7cbd690178239c2d60ea2cefd1ba7731c307
Stephen Hess <stephen.k.hess@gmail.com>
2017-04-17 14:20:38 -0400

    return error message on 500

--- server/routes/search.js
+++ server/routes/search.js
@@ -16,7 +16,7 @@ module.exports = function( req, res ){
 
   // fetch all result docs by id
   ph.store.getMany( ids, function( err, results ){
-    if( err ){ return res.status(500).send({}); }
+    if( err ){ return res.status(500).send(err); }
     if( !results || !results.length ){ return res.status(200).send([]); }
 
     // get a list of parent ids
