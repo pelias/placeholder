dadd5729c2f07e0920d9ce1ab05b9b9d8f16aa1c
Diana Shkolnikov <dianashk@gmail.com>
2017-07-14 12:01:50 -0400

    set working dir properly in new scripts

---
+++ cmd/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
+
+PLACEHOLDER_DATA=${PLACEHOLDER_DATA:-"./data"};
+
+rm -f ${PLACEHOLDER_DATA}/graph.json ${PLACEHOLDER_DATA}/store.sqlite3;
+
+cat ${PLACEHOLDER_DATA}/wof.extract | node ${DIR}/load.js
+
+echo 'Done!'
