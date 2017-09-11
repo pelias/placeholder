20aa8b2e13d6fe340d55cade6ca7bdbb9ef4fd9a
Julian Simioni <julian@mapzen.com>
2017-09-11 13:52:14 -0400

    Skip Docker image builds for Greenkeeper branches
    We have found that builds for Greenkeeper branches fail because the
    branch names have characters in them that are not valid in a Docker
    image tag. Fortunately, we also don't really want these branches to
    result in CI built Docker images anyway, so an easy workaround is to
    skip building Docker images on those branches.
    
    Connects https://github.com/pelias/dockerfiles/issues/21

--- .circleci/docker.sh
+++ .circleci/docker.sh
@@ -6,6 +6,11 @@ DATE=`date +%Y-%m-%d`
 DOCKER_REPOSITORY="pelias"
 DOCKER_PROJECT="${DOCKER_REPOSITORY}/${CIRCLE_PROJECT_REPONAME}"
 
+# skip builds on greenkeeper branches
+if [ "${CIRCLE_BRANCH##greenkeeper}" ]; then
+	exit 0
+fi
+
 # the name of the image that represents the "branch", that is an image that will be updated over time with the git branch
 # the production branch is changed to "latest", otherwise the git branch becomes the name of the version
 if [[ "${CIRCLE_BRANCH}" == "production" ]]; then
