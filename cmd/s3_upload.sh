70581187e15486a0b77cc08f66640c96f972506c
missinglink <insomnia@rcpt.at>
2017-09-22 14:29:23 +0200

    feat(s3_upload) - archive builds in s3

--- cmd/s3_upload.sh
+++ cmd/s3_upload.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 DATA_DIR=${PLACEHOLDER_DATA:-"${DIR}/../data"};
 BUCKET='s3://pelias-data/placeholder/';
+TODAY=`date +%Y-%m-%d`;
 
 echo '--- gzip data files ---';
 if type pigz >/dev/null
@@ -23,8 +24,13 @@ aws s3 cp "${DATA_DIR}/graph.json.gz" "${BUCKET}" --region us-east-1 --acl publi
 aws s3 cp "${DATA_DIR}/store.sqlite3.gz" "${BUCKET}" --region us-east-1 --acl public-read;
 aws s3 cp "${DATA_DIR}/wof.extract.gz" "${BUCKET}" --region us-east-1 --acl public-read;
 
+echo '--- create archive ---';
+aws s3 cp "${BUCKET}graph.json.gz" "${BUCKET}archive/${TODAY}/graph.json.gz" --region us-east-1 --acl public-read;
+aws s3 cp "${BUCKET}store.sqlite3.gz" "${BUCKET}archive/${TODAY}/store.sqlite3.gz" --region us-east-1 --acl public-read;
+aws s3 cp "${BUCKET}wof.extract.gz" "${BUCKET}archive/${TODAY}/wof.extract.gz" --region us-east-1 --acl public-read;
+
 echo '--- list files ---';
 aws s3 ls --human-readable "${BUCKET}";
-# 2017-05-05 15:19:08   13.3 MiB graph.json.gz
-# 2017-05-05 15:19:48   43.2 MiB store.sqlite3.gz
-# 2017-05-05 15:20:30   46.0 MiB wof.extract.gz
+
+echo '--- list archive files ---';
+aws s3 ls --human-readable "${BUCKET}archive/${TODAY}/";
