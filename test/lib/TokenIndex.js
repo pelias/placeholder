716f44b56d1ff2091169acb5d4b3fa295d7a66a6
missinglink <insomnia@rcpt.at>
2017-11-21 13:39:26 +0100

    revert adding placetype column

--- test/lib/TokenIndex.js
+++ test/lib/TokenIndex.js
@@ -30,10 +30,9 @@ module.exports.reset = function(test, common) {
     sql = 'PRAGMA table_info(tokens)';
     t.deepEqual( db.prepare(sql).all(), [
       { cid: 0, name: 'id', type: 'INTEGER', notnull: 0, dflt_value: null, pk: 0 },
-      { cid: 1, name: 'layer', type: 'STRING', notnull: 0, dflt_value: null, pk: 0 },
-      { cid: 2, name: 'lang', type: 'STRING', notnull: 0, dflt_value: null, pk: 0 },
-      { cid: 3, name: 'tag', type: 'STRING', notnull: 0, dflt_value: null, pk: 0 },
-      { cid: 4, name: 'token', type: 'STRING', notnull: 0, dflt_value: null, pk: 0 }
+      { cid: 1, name: 'lang', type: 'STRING', notnull: 0, dflt_value: null, pk: 0 },
+      { cid: 2, name: 'tag', type: 'STRING', notnull: 0, dflt_value: null, pk: 0 },
+      { cid: 3, name: 'token', type: 'STRING', notnull: 0, dflt_value: null, pk: 0 }
     ]);
 
     // ensure table has been created
@@ -176,8 +175,8 @@ module.exports.setTokens = function(test, common) {
       // ensure rows have been created
       const sql = 'SELECT * FROM tokens';
       t.deepEqual( db.prepare(sql).all(), [
-        { id: 100, layer: null, lang: 'en', tag: 'abbr', token: 'test1' },
-        { id: 100, layer: null, lang: 'fr', tag: 'variant', token: 'test2' }
+        { id: 100, lang: 'en', tag: 'abbr', token: 'test1' },
+        { id: 100, lang: 'fr', tag: 'variant', token: 'test2' }
       ]);
 
     });
