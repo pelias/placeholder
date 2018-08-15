c7daaf7590401e9a4bf940d02927d8ec96099deb
Julian Simioni <julian@simioni.org>
2018-08-15 08:30:27 -0400

    Fix circle branch handling
    This fixes Circle so that it can build tags for branches with a slash in
    the branch name.

--- .circleci/docker.sh
+++ .circleci/docker.sh
@@ -6,22 +6,19 @@ DATE=`date +%Y-%m-%d`
 DOCKER_REPOSITORY="pelias"
 DOCKER_PROJECT="${DOCKER_REPOSITORY}/${CIRCLE_PROJECT_REPONAME}"
 
-# skip builds on greenkeeper branches
-if [[ -z "${CIRCLE_BRANCH##*greenkeeper*}" ]]; then
-	exit 0
-fi
+BRANCH="$(echo $CIRCLE_BRANCH | tr '/' '-')" #slashes are not valid in docker tags. replace with dashes
 
 # the name of the image that represents the "branch", that is an image that will be updated over time with the git branch
 # the production branch is changed to "latest", otherwise the git branch becomes the name of the version
-if [[ "${CIRCLE_BRANCH}" == "production" ]]; then
+if [[ "${BRANCH}" == "production" ]]; then
 	DOCKER_BRANCH_IMAGE_VERSION="latest"
 else
-	DOCKER_BRANCH_IMAGE_VERSION="${CIRCLE_BRANCH}"
+	DOCKER_BRANCH_IMAGE_VERSION="$BRANCH"
 fi
 DOCKER_BRANCH_IMAGE_NAME="${DOCKER_PROJECT}:${DOCKER_BRANCH_IMAGE_VERSION}"
 
 # the name of the image that represents the "tag", that is an image that is named with the date and git commit and will never be changed
-DOCKER_TAG_IMAGE_VERSION="${CIRCLE_BRANCH}-${DATE}-${CIRCLE_SHA1}"
+DOCKER_TAG_IMAGE_VERSION="${BRANCH}-${DATE}-${CIRCLE_SHA1}"
 DOCKER_TAG_IMAGE_NAME="${DOCKER_PROJECT}:${DOCKER_TAG_IMAGE_VERSION}"
 
 # build image and login to docker hub
