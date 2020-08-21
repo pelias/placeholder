56b2ffa8b51e5658a30111d7fbebcbf0b7fd38de
missinglink <insomnia@rcpt.at>
2020-08-21 13:17:29 +0200

    fix(slow_query): rewrite match_subject_object.sql

--- query/match_subject_object.sql
+++ query/match_subject_object.sql
@@ -1,16 +1,28 @@
-SELECT t1.id AS subjectId, t2.id as objectId
-FROM lineage AS l1
-  JOIN tokens AS t1 ON t1.id = l1.id
-  JOIN tokens AS t2 ON t2.id = l1.pid
-WHERE t1.token = $subject
-AND t2.token = $object
-AND (
+WITH l AS (
+  SELECT *
+  FROM lineage
+  WHERE id IN (
+    SELECT id
+    FROM tokens
+    WHERE token = $subject
+  )
+  AND pid IN (
+    SELECT id
+    FROM tokens
+    WHERE token = $object
+  )
+)
+SELECT l.id AS subjectId, l.pid as objectId
+FROM l
+  JOIN tokens AS t1 USING (id)
+  JOIN tokens AS t2 ON t2.id = l.pid
+WHERE (
   t1.lang = t2.lang OR
   t1.lang IN ( 'eng', 'und' ) OR
   t2.lang IN ( 'eng', 'und' )
 )
 -- AND t1.tag NOT IN ( 'colloquial' )
 -- AND t2.tag NOT IN ( 'colloquial' )
-GROUP BY t1.id, t2.id
-ORDER BY t1.id ASC, t2.id ASC
+GROUP BY l.id, l.pid
+ORDER BY l.id ASC, l.pid ASC
 LIMIT $limit
