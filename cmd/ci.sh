049f9fdb690cab514334eed789ac49655fad0f6c
missinglink <insomnia@rcpt.at>
2021-11-04 13:09:29 +0100

    fix(ci): remove "archive" path prefix from fixture download URL

--- cmd/ci.sh
+++ cmd/ci.sh
@@ -4,7 +4,7 @@ BUCKET=https://data.geocode.earth/placeholder
 export AGENT="github/${GITHUB_ACTOR}"
 export REFERER="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
 mkdir data
-curl -A ${AGENT} -e ${REFERER} -sfo data/store.sqlite3.gz ${BUCKET}/archive/$(date +%Y-%m-%d)/store.sqlite3.gz || true
+curl -A ${AGENT} -e ${REFERER} -sfo data/store.sqlite3.gz ${BUCKET}/$(date +%Y-%m-%d)/store.sqlite3.gz || true
 [ -e data/store.sqlite3.gz ] || curl -A ${AGENT} -e ${REFERER} -so data/store.sqlite3.gz ${BUCKET}/store.sqlite3.gz
 gunzip data/store.sqlite3.gz
 
